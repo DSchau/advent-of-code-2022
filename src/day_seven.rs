81ffabbf5d3316e31ca8eae6e1bad157620d121b
Dustin Schau <dustinschau@gmail.com>
2022-12-26 10:51:24 -0600

    chore: finish day 7

--- src/day_seven.rs
+++ src/day_seven.rs
@@ -1,5 +1,80 @@
-pub fn day_seven(input: String) -> i32 {
-    -1
+use std::{collections::HashMap, path::PathBuf};
+
+// thank you https://github.com/NickyMeuleman/scrapyard/blob/main/advent_of_code/2022/src/day_07.rs
+pub fn part_one(input: String) -> String {
+    let mut sizes = HashMap::new();
+    let mut affected = Vec::new();
+
+    for line in input.lines() {
+        if line.starts_with("$ ls") || line.starts_with("dir") {
+            continue;
+        }
+
+        let parts: Vec<_> = line.split_whitespace().collect();
+        match parts[..] {
+            ["$", "cd", ".."] => {
+                affected.pop();
+            }
+            ["$", "cd", name] => {
+                affected.push(name);
+            }
+            [size, _name] => {
+                let size: u32 = size.parse().unwrap();
+                for idx in 0..affected.len() {
+                    let path = PathBuf::from_iter(&affected[..=idx]);
+                    *sizes.entry(path).or_insert(0) += size;
+                }
+            }
+            _ => {}
+        };
+    }
+
+    sizes
+        .into_values()
+        .filter(|size| *size <= 100_000)
+        .sum::<u32>()
+        .to_string()
+}
+
+fn part_two(input: String) -> String {
+    let mut sizes = HashMap::new();
+    let mut affected = Vec::new();
+
+    for line in input.lines() {
+        if line.starts_with("$ ls") || line.starts_with("dir") {
+            continue;
+        }
+
+        let parts: Vec<_> = line.split_whitespace().collect();
+        match parts[..] {
+            ["$", "cd", ".."] => {
+                affected.pop();
+            }
+            ["$", "cd", name] => {
+                affected.push(name);
+            }
+            [size, _name] => {
+                let size: u32 = size.parse().unwrap();
+                for idx in 0..affected.len() {
+                    let path = PathBuf::from_iter(&affected[..=idx]);
+                    *sizes.entry(path).or_insert(0) += size;
+                }
+            }
+            _ => {}
+        };
+    }
+
+    let disk = 70_000_000;
+    let needed = 30_000_000;
+    let root = sizes.get(&PathBuf::from("/")).unwrap();
+    let available = disk - root;
+
+    sizes
+        .into_values()
+        .filter(|size| available + size >= needed)
+        .min()
+        .unwrap()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -9,9 +84,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sample_day_seven() {
+    fn test_get_tree_day_seven_sample() {
         let input = utils::read_test_file("day_seven", "sample.txt");
 
-        assert_eq!(day_seven(input), -1); // 95437
+        let result = part_one(input);
+
+        assert_eq!(result, "95437");
+    }
+
+    #[test]
+    fn test_sample_day_seven_input() {
+        let input = utils::read_test_file("day_seven", "puzzle.txt");
+
+        let result = part_one(input);
+
+        assert_eq!(result, "1086293");
+    }
+
+    #[test]
+    fn test_sample_day_seven_input_part_two() {
+        let input = utils::read_test_file("day_seven", "puzzle.txt");
+
+        let result = part_two(input);
+
+        assert_eq!(result, "366028");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod day_five;
 mod day_four;
 mod day_one;
+mod day_seven;
 mod day_six;
 mod day_three;
 mod day_two;
-mod day_seven;
 
 mod utils;
 
